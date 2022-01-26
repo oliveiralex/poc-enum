17275f9ed3dc7ec18cd970e2a1ac48156ecef127
Alexandre Oliveira <alexandre.si.ufu@gmail.com>
2022-01-26 10:01:20 -0300

    project created

---
+++ src/main/java/com/educandoweb/pocEnum/PocEnumApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.pocEnum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocEnumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocEnumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educandoweb/pocEnum/PocEnumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.pocEnum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocEnumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
